361a4c16bc3b54a2db091bc7a73df971ee5f8a51
Sean Hsieh <seanyen0507@gmail.com>
2014-12-29 01:34:03 +0800

    modify Rakefile and demo_sns_pub

--- Rakefile
+++ Rakefile
@@ -5,7 +5,7 @@ namespace :db do
   desc "Create data store"
   task :migrate do
     begin
-      Notification.create_table(5, 6)
+      NBANotify.create_table(5, 6)
       puts "New data store created"
     rescue AWS::DynamoDB::Errors::ResourceInUseException => e
       puts 'Data store exists -- no changes made, no retry attempted'
--- demo_sns_pub.rb
+++ demo_sns_pub.rb
@@ -1,9 +1,9 @@
 require 'aws-sdk'
 
-topic_arn = 'arn:aws:sns:us-west-2:700793290995:sns_plyer'
+topic_arn = 'arn:aws:sns:us-west-2:700793290995:sns_player'
 
 sns = AWS::SNS.new
 t = sns.topics[topic_arn]
-result = t.publish('The 5th of November', subject: 'Remember Remember')
+result = t.publish('Kobe_bryant', subject: 'Player Ranking')
 
 puts result
